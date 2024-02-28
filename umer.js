71f4538ecb28abb99e96e955aa590f39c33daa60
umerkashmiri90 <160102219+umerkashmiri90@users.noreply.github.com>
2024-02-28 14:25:36 +0500

    Add files via upload

--- umer.js
+++ umer.js
@@ -1,1 +1,2 @@
-console.log("assign one");
+var personName = "assign two";
+console.log("Hello ".concat(personName, " would you like to some words today?\""));
--- umer.ts
+++ umer.ts
@@ -1,1 +1,2 @@
-console.log("assign one");
+let personName: string = "assign two"
+console.log(`Hello ${personName} would you like to some words today?"`);
